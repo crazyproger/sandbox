589a3acdaa514aad863b256f605a673847547a8d
crazyproger <vladimir.rudev@gmail.com>
2012-12-01 01:08:08 +0400

    added webtoper module

---
+++ webtoper/src/a/b/c/d/Test.java
@@ -0,0 +1,11 @@
+package a.b.c.d;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ice
+ * Date: 11/17/12
+ * Time: 11:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
